6e44cda7a3ffeb7ede85601a226e942a68fa4e4e
samira <samiraabdiurmia@gmail.com>
2022-09-16 17:48:43 -0700

    feat (IKEA) : update navbar

--- src/Layouts/MainLayout/Navbar/Navbar.tsx
+++ src/Layouts/MainLayout/Navbar/Navbar.tsx
@@ -11,14 +11,17 @@ const Navbar = () => {
    
  
 <header className="w-screen p-3 bg-white shadow fixed">
-    <div>
-    <nav className="flex justify-between items-center">
-    <img src={images['Navbar/logo.png']} alt="IKEA logo" width="60" height="150" loading="lazy" />
+    <div className="flex py-3">
+    <img src={images['Navbar/logo.png']} alt="IKEA logo" width="60" height="150" loading="lazy" className="flex-none px-3  w-24 -mt-1"/>
+    <nav className="flex  Space evenly flex-initial w-screen ">
+  
 
     {item.map((item) => (
-            <a href={item.route} className="hover:underline font-semibold text-slate-900">  {item.name}</a>
+            <a href={item.route} className="px-3 hover:underline font-semibold text-slate-900">  {item.name}</a>
             ))}
+            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
             </nav>
+            <a className="focus:bg-violet-700 flex-initial w-40 px-3 hover:underline font-semibold text-slate-900">Shop at IKEA</a>
             </div>
 </header>
 
