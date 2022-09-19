c595fd9c226216653d4da5e719974cf3f473c819
shayanDoroodian <doroodianshayan64@gmail.com>
2022-09-19 16:03:16 +0300

    feat(lifeAtHome) : create life at home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,54 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ScrollToTop } from "./Components";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ScrollToTop } from './Components';
 
 import {
-  About,
-  Home,
-  Login,
-Behind,
-InternationalSales,
-LifeAtHome,
-Museum,
-WorkWithUs,
-Newsroom,
-Sustainability,
-Contact,
-} from "./Pages/Main";
+	About,
+	Home,
+	Login,
+	Behind,
+	InternationalSales,
+	LifeAtHome,
+	Museum,
+	WorkWithUs,
+	Newsroom,
+	Sustainability,
+	Contact,
+} from './Pages/Main';
 
-import MainLayout from "./Layouts/MainLayout/MainLayout";
+import MainLayout from './Layouts/MainLayout/MainLayout';
 
-import HomeMainLayout from "./Layouts/MainLayout/HomeMainLayout"
-
-
+import HomeMainLayout from './Layouts/MainLayout/HomeMainLayout';
 
 function App() {
-  return (
-    <>
-      <Router>
-      <ScrollToTop>
-        <Routes>
-        <Route element={<HomeMainLayout />}>
-          <Route path="/" element={<Home />} />
-        
+	return (
+		<>
+			<Router>
+				<ScrollToTop>
+					<Routes>
+						<Route element={<HomeMainLayout />}>
+							<Route path='/' element={<Home />} />
+						</Route>
+						<Route element={<MainLayout />}>
+							<Route path='/about' element={<About />} />
+							<Route path='/login' element={<Login />} />
+							<Route path='/behind' element={<Behind />} />
+							<Route
+								path='/international-sales'
+								element={<InternationalSales />}
+							/>
+							<Route path='/life-at-home' element={<LifeAtHome />} />
+							<Route path='/museum' element={<Museum />} />
+							<Route path='/work-with-us' element={<WorkWithUs />} />
+							<Route path='/museum' element={<Museum />} />
 
-
-
-          
-          </Route>
-        <Route element={<MainLayout />}>
-         
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/behind" element={<Behind />} />
-          <Route path="/international-sales" element={<InternationalSales />} />
-          <Route path="/life-at-home" element={<LifeAtHome />} />
-          <Route path="/museum" element={<Museum />} />
-          <Route path="/work-with-us" element={<WorkWithUs />} />
-          <Route path="/museum" element={<Museum />} />
-
-          <Route path="/museum" element={<Museum />} />
-
-
-
-          
-          </Route>
-          
-
-         
-         
-         
-           
-          
-        </Routes>
-        </ScrollToTop>
-      </Router>
-    </>
-  );
+							<Route path='/museum' element={<Museum />} />
+						</Route>
+					</Routes>
+				</ScrollToTop>
+			</Router>
+		</>
+	);
 }
 
 export default App;
--- src/Pages/Main/LifeAtHome/LifeAtHome.tsx
+++ src/Pages/Main/LifeAtHome/LifeAtHome.tsx
@@ -1,11 +1,138 @@
+function LifeAtHome() {
+	return (
+		<div className='w-full pt-32'>
+			<div className='m-auto  w-2/3 mb-12'>
+				<h1 className='text-5xl	font-bold mb-6'>
+					Creating a better life at home
+				</h1>
+				<p>
+					If there’s one thing we’ve learnt over the past 75+ years, it’s that a
+					better home creates a better life. So we work to create better homes,
+					every day, through the products and solutions we offer.
+				</p>
+			</div>
+			<div className='flex w-7/12 m-auto mt-24 mb-44'>
+				<div className='w-1/2 mr-12'>
+					<img
+						className='flex w-full'
+						src='https://gbl-sc9u2-prd-cdn.azureedge.net/-/media/aboutikea/images/life-at-home/a-woman-in-a-room-ai05-01-ph161124-ikea-l.jpg?rev=06963ebc27cc47d3b3d8d8c530fe6a7f'
+						alt=''
+					/>
+				</div>
 
+				<div className='w-1/2 ml-12'>
+					<h2 className='text-4xl	font-bold mb-6'>Our curiosity box</h2>
+					<p className='mb-6'>
+						To create better homes, we need to know the challenges people are
+						facing in their everyday lives. The world is changing, faster than
+						ever before, and so are people’s needs and dreams.
+					</p>
+					<a
+						href='https://about.ikea.com/en/life-at-home/curiosity-box'
+						className='text-blue-800 text-lg font-semibold'>
+						See how we are keeping up with life at home
+					</a>
+				</div>
+			</div>
+			<div className=' mb-12 bg-gray-100 w-full py-16'>
+				<div className='m-auto w-7/12 mb-12 bg-gray-100'>
+					<h1 className='text-5xl	font-bold mb-12'>Did you know?</h1>
+					<p className='text-4xl mb-16'>
+						Every year since 2014 we’ve been connecting with people around the
+						world to create our Life at Home Report.
+					</p>
+					<p className='text-4xl mb-8'>
+						So far, we’ve spoken to 105 738 of them.
+					</p>
+					<a
+						href='https://lifeathome.ikea.com/'
+						className='text-blue-800 text-lg font-semibold	'>
+						See how we are keeping up with life at home
+					</a>
+				</div>
+			</div>
+			<div className='flex w-7/12 m-auto mt-24 mb-44'>
+				<div className='w-1/2 mr-12'>
+					<img
+						className='flex w-full'
+						src='https://gbl-sc9u2-prd-cdn.azureedge.net/-/media/aboutikea/images/life-at-home/a-woman-in-a-room-ai05-01-ph161124-ikea-l.jpg?rev=06963ebc27cc47d3b3d8d8c530fe6a7f'
+						alt=''
+					/>
+				</div>
 
-function LifeAtHome(){
-    return (
-        <>
-        <div>LifeAtHome</div>
-        </>
-    )
+				<div className='w-1/2 ml-12'>
+					<h2 className='text-4xl	font-bold mb-6'>How we work</h2>
+					<p className='mb-6'>
+						Finding the smartest, most innovative, cost-efficient way of doing
+						things is part of our DNA. We’ve always gone our own way, knowing
+						that you can’t blaze a trail by walking the same path as everyone
+						else.
+					</p>
+					<a
+						href='https://gbl-sc9u2-prd-cdn.azureedge.net/-/media/aboutikea/images/life-at-home/smart-symfonisk-lamp-ai05-03-pe820515-ikea-l-a3c9b31d/smart-symfonisk-lamp-ai05-03-pe820515-ikea-l-a3c9b31d-figure-image-mobile?rev=dff9b89ae40f47d5903dfe5a40f7a311'
+						className='text-blue-800 text-lg font-semibold'>
+						Find out how we create safer, smarter, more beautiful homes
+					</a>
+				</div>
+			</div>
+			<div className='flex w-7/12 m-auto mt-24 mb-44'>
+				<div className='w-1/2 mr-12'>
+					<img
+						className='flex w-full'
+						src='https://gbl-sc9u2-prd-cdn.azureedge.net/-/media/aboutikea/images/life-at-home/young-woman-leaning-on-desk-holding-take-away-food-ai05-ph161073-ikea-c8a466b5/young-woman-leaning-on-desk-holding-take-away-food-ai05-ph161073-ikea-c8a466b5-figure-image-mobile?rev=7fbd7b6767c74764b373263f3e23b2a9'
+						alt=''
+					/>
+				</div>
+
+				<div className='w-1/2 ml-12'>
+					<h2 className='text-4xl	font-bold mb-6'>
+						Co-creation - working with you to make better things
+					</h2>
+					<p className='mb-6'>
+						We believe that the best things are those we create together.
+						Through co-creation, we invite you to share your ideas and feedback
+						with us.
+					</p>
+					<a
+						href='https://about.ikea.com/en/life-at-home/co-creation'
+						className='text-blue-800 text-lg font-semibold'>
+						Learn more and share your idea
+					</a>
+				</div>
+			</div>
+			<div className=' mb-12 bg-gray-100 w-full py-16 '>
+				<div className='m-auto w-7/12 mb-12 bg-gray-100 flex justify-end relative'>
+					<div className='w-1/2 pr-24 absolute left-0 top-0 bg-gray-100 '>
+						<img
+							src='https://gbl-sc9u2-prd-cdn.azureedge.net/-/media/aboutikea/images/about-us/making-homes-better/democratic-design/sarah-fager-senior-designer-ikea-of-sweden-ai010301-ikea-s?rev=b2518a145267422c85e192e32d39b3e1'
+							alt=''
+							className='w-full'
+						/>
+						<div className='p-6'>
+							<h2 className='text-2xl	font-bold mb-6'>
+								Democratic Design. Making great design available to everyone.
+							</h2>
+							<p>
+								The five dimensions of Democratic Design are at the heart of our
+								products. Find out what they are and how we use them.
+							</p>
+							<button>-</button>
+						</div>
+					</div>
+					<div className='w-1/2 pl-24 '>
+						<img
+							className='w-full'
+							src='https://gbl-sc9u2-prd-cdn.azureedge.net/-/media/aboutikea/images/life-at-home/five-democratic-design-principles-ai05-04-px104231-ikea-l.jpg?rev=e178460d10c54b7d8fc61384f0e083e2'
+							alt=''
+						/>
+						<p className='text-gray-600 text-sm mt-2'>
+							The five dimensions of Democratic Design.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default LifeAtHome;
